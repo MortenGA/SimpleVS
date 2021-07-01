5d57cdca186f1e138f4a503f5aae0edb1083c2fd
Morten <zemik2004@gmail.com>
2021-07-01 12:54:42 +0300

    Wrote some code

---
+++ SimpleC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SimpleC
+{
+
+
+    
+     
+   
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = 10;
+            Console.WriteLine(s.GetType());
+            Console.WriteLine("Some Code");
+        }
+    }
+}
